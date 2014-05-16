6c5ef0b4d4e1e8d4752bb1d35f4040318eae942d
MARCUS HSU <kiddkevin01@gmail.com>
2014-05-16 02:38:05 -0400

    update

--- src/com/example/coolstocktool/SearchStockActivity.java
+++ src/com/example/coolstocktool/SearchStockActivity.java
@@ -1,16 +1,39 @@
 package com.example.coolstocktool;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class SearchStockActivity extends ActionBarActivity {
+
+	public Button _searchStockButton;
+	public Context context;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_search_stock);
+
+		context = this;
+
+		_searchStockButton = (Button) findViewById(R.id.searchStock);
+		_searchStockButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent();
+				intent.setClass(context, TopicForAnStockActivity.class);
+				intent.putExtra("msg3", "from search stock activity");
+				startActivity(intent);
+			}
+		});
 
 	}
 
