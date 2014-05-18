ba334b4f622e07c238fe9633ca4ce2751ae97bac
MARCUS HSU <kiddkevin01@gmail.com>
2014-05-17 21:12:23 -0400

    Starting Point

--- gen/com/example/coolstocktool/R.java
+++ gen/com/example/coolstocktool/R.java
@@ -1099,21 +1099,24 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f050055;
+        public static final int action_settings=0x7f05005b;
         public static final int activity_chat_room=0x7f05003f;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int activity_main=0x7f050041;
         public static final int activity_search_stock=0x7f050048;
         public static final int activity_second=0x7f05003c;
         public static final int activity_stock_detail=0x7f05004b;
-        public static final int activity_topic_for_an_stock=0x7f05004f;
+        public static final int activity_topic_for_an_stock=0x7f050050;
         public static final int addBookmark=0x7f05004c;
-        public static final int addThread=0x7f050051;
+        public static final int addThread=0x7f050052;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
+        public static final int createevent=0x7f050056;
+        public static final int dateEditText=0x7f05005a;
         public static final int default_activity_button=0x7f050028;
+        public static final int delete=0x7f05004f;
         public static final int dialog=0x7f05000e;
         public static final int disableHome=0x7f050008;
         public static final int dropdown=0x7f05000f;
@@ -1127,13 +1130,15 @@ containing a value of this type.
         public static final int icon=0x7f05002a;
         public static final int ifRoom=0x7f05000a;
         public static final int image=0x7f050027;
-        public static final int imageView1=0x7f050053;
-        public static final int item=0x7f050052;
-        public static final int layout1=0x7f050054;
+        public static final int imageView1=0x7f050054;
+        public static final int item=0x7f050053;
+        public static final int layout1=0x7f050055;
         public static final int listMode=0x7f050001;
         public static final int listView1=0x7f05003d;
         public static final int list_item=0x7f050029;
         public static final int middle=0x7f050012;
+        public static final int nameEditText=0x7f050059;
+        public static final int nameText=0x7f050057;
         public static final int never=0x7f050009;
         public static final int none=0x7f050010;
         public static final int normal=0x7f050000;
@@ -1166,13 +1171,14 @@ containing a value of this type.
         public static final int test=0x7f050047;
         public static final int textView1=0x7f050045;
         public static final int textView2=0x7f050046;
-        public static final int threadList=0x7f050050;
+        public static final int threadList=0x7f050051;
         public static final int title=0x7f05002b;
         public static final int toBM=0x7f050042;
         public static final int top_action_bar=0x7f050020;
         public static final int ueserID=0x7f050043;
         public static final int up=0x7f050021;
         public static final int useLogo=0x7f050003;
+        public static final int whenText=0x7f050058;
         public static final int withText=0x7f05000c;
     }
     public static final class integer {
@@ -1235,7 +1241,8 @@ containing a value of this type.
         public static final int adapter_bm=0x7f03001e;
         public static final int adapter_chat_room=0x7f03001f;
         public static final int adapter_topic_for_an_stock=0x7f030020;
-        public static final int support_simple_spinner_dropdown_item=0x7f030021;
+        public static final int createevent=0x7f030021;
+        public static final int support_simple_spinner_dropdown_item=0x7f030022;
     }
     public static final class menu {
         public static final int chat_room_adapter=0x7f0c0000;
--- src/com/example/coolstocktool/TopicForAnStockActivity.java
+++ src/com/example/coolstocktool/TopicForAnStockActivity.java
@@ -1,19 +1,24 @@
 package com.example.coolstocktool;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import android.R.integer;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
-import android.content.DialogInterface.OnClickListener;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import com.stockcloud.ChatTopic;
@@ -35,12 +40,18 @@ public class TopicForAnStockActivity extends ActionBarActivity {
 	public ListView _threadList;
 	public Context _context;
 	public TopicForAnStockAdapter _adapter;
+	public Button _addThread;
 
 	// public ArrayList<DataForm2> data;
 	public TopicAsyncTask topicAsync;
 	public String stockName;
 	public String email;
 	public String password;
+	public AddTopicAsyncTask addTopicAsync;
+	public ChatTopic cTopic;
+	// for dialog
+	public EditText _title;
+	public EditText _detail;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -48,6 +59,7 @@ public class TopicForAnStockActivity extends ActionBarActivity {
 		setContentView(R.layout.activity_topic_for_an_stock);
 
 		_context = this;
+		_addThread = (Button) findViewById(R.id.addThread);
 
 		Intent intent = getIntent();
 		stockName = intent.getStringExtra("stockName");
@@ -72,6 +84,59 @@ public class TopicForAnStockActivity extends ActionBarActivity {
 
 		topicAsync = new TopicAsyncTask();
 		topicAsync.execute(stockName);
+
+		_addThread.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// 1. Instantiate an AlertDialog.Builder with its constructor
+				AlertDialog.Builder builder = new AlertDialog.Builder(_context);
+
+				// 2. Chain together various setter methods to set the dialog
+				// characteristics
+				builder.setTitle("Partner Finding..");
+
+				// 3. setView in the middle of dialog
+				LayoutInflater inflater = getLayoutInflater();
+				final View view = inflater.inflate(R.layout.createevent, null);
+				builder.setView(view);
+				// 4. Add the buttons
+				builder.setPositiveButton("Create",
+						new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog, int id) {
+								// User clicked CREATE button
+								_title = (EditText) view
+										.findViewById(R.id.nameEditText);
+								_detail = (EditText) view
+										.findViewById(R.id.dateEditText);
+
+								List<String> paras = new ArrayList<String>();
+								paras.add(stockName);
+								paras.add(_title.getText().toString());
+								paras.add(_detail.getText().toString());
+
+								addTopicAsync = new AddTopicAsyncTask();
+								// addTopicAsync.execute(paras);
+
+								// return to background
+								dialog.dismiss();
+							}
+						});
+				builder.setNegativeButton("Cancel",
+						new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog, int id) {
+								// User cancelled the dialog
+								dialog.dismiss();
+							}
+						});
+				// 5. Get the AlertDialog from create()
+				AlertDialog dialog = builder.create();
+				// 6. put this to where ever you want to show this dialog
+				dialog.show();
+
+			}
+
+		});
 
 	}
 
@@ -122,23 +187,42 @@ public class TopicForAnStockActivity extends ActionBarActivity {
 				_threadList = (ListView) findViewById(R.id.threadList);
 				_threadList.setAdapter(_adapter);
 			} else {
-				// system alert dialog
-				AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
-						_context);
-				alertDialogBuilder.setTitle("Sorry!!");
-				alertDialogBuilder.setMessage("Nothing Found!!!")
-						.setPositiveButton("Try Another Name",
-								new OnClickListener() {
 
-									@Override
-									public void onClick(DialogInterface dialog,
-											int which) {
-										onBackPressed();
-									}
-								});
-				AlertDialog alertDialog = alertDialogBuilder.create();
+			}
+		}
 
-				alertDialog.show();
+	}
+
+	// for testing
+	private class AddTopicAsyncTask extends
+			AsyncTask<List<String>, integer, String> {
+
+		@Override
+		protected String doInBackground(List<String>... v) {
+
+			try {
+				Log.d("***",
+						"Input6: " + v[0].get(0) + v[0].get(1) + v[0].get(2));
+				cTopic = new ChatTopic();
+				cTopic = cTopic.findChatTopic("Instamour");
+				cTopic.addThreadToTopic("title", "text", "user_email");
+
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return "okay";
+		}
+
+		// for getView method
+		@Override
+		protected void onPostExecute(String result) {
+
+			if (result != null) {
+				Log.d("*****", "onpost result2: " + result);
+
+			} else {
+
 			}
 		}
 
