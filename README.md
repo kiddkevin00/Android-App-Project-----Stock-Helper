d567c91509559175726af27e0fd98cfb182fc7b3
MARCUS HSU <kiddkevin01@gmail.com>
2014-05-21 18:09:02 -0400

    update

---
+++ README.md
@@ -0,0 +1,36 @@
+Stock Helper
+============
+
+## Demo video:
+https://www.youtube.com/watch?v=tR6mGoPRgAo&feature=youtu.be
+
+## Motication:  
+- People want to make sure that the information about stock is not only updated and but reliable.
+- People might hope to have some straight-forward predicted price, which has alrealy been caculated by some complex algorithm.
+
+## Use Case:
+- People can cath up the most updated not only information but prediction every moment by simply clicking a button.
+- People are able get exprienced stock stock traders's advice by going to the forum of each stock.
+
+## Functions
+- Chat room for each topic within each stock
+- Voting result for each message
+- BookMarked favorate stocks
+- Log in credential
+- Updated information and Predicted stock price
+
+## Implementation:
+- Retreive updated Stock Info(Yahoo Stock API)
+- Store user's info and activity in back-end server(AWS)
+- Implement some prediction Algorithm(polynomial Regression)
+- Integrate back-end function into Android development(multi-thread)
+
+## Future Work
+- Support more Prediction Algorithm
+- Support Facebook account Log-in
+- Implement some animation decorating UI
+- Accelerate Server data computation
+
+## How to run it
+Simply clone the project to local repository. After enter AWS credential including Secret Key and Access Key in '''AwsCredentials.properties''', then run it either on simulator or any Android device. 
+
--- gen/com/example/coolstocktool/R.java
+++ gen/com/example/coolstocktool/R.java
@@ -858,12 +858,6 @@ containing a value of this type.
         public static final int abc_split_action_bar_is_narrow=0x7f060002;
     }
     public static final class color {
-<<<<<<< HEAD
-        public static final int abc_search_url_text_holo=0x7f070003;
-        public static final int abc_search_url_text_normal=0x7f070000;
-        public static final int abc_search_url_text_pressed=0x7f070002;
-        public static final int abc_search_url_text_selected=0x7f070001;
-=======
         public static final int abc_search_url_text_holo=0x7f07000b;
         public static final int abc_search_url_text_normal=0x7f070000;
         public static final int abc_search_url_text_pressed=0x7f070002;
@@ -876,7 +870,6 @@ containing a value of this type.
         public static final int offwhite=0x7f07000a;
         public static final int white=0x7f070004;
         public static final int yellow=0x7f070009;
->>>>>>> Marcus
     }
     public static final class dimen {
         /**  Default height of an action bar. 
@@ -1092,14 +1085,10 @@ containing a value of this type.
         public static final int abc_textfield_searchview_holo_light=0x7f020054;
         public static final int abc_textfield_searchview_right_holo_dark=0x7f020055;
         public static final int abc_textfield_searchview_right_holo_light=0x7f020056;
-<<<<<<< HEAD
-        public static final int ic_launcher=0x7f020057;
-=======
         public static final int background=0x7f020057;
         public static final int background2=0x7f020058;
         public static final int ic_launcher=0x7f020059;
         public static final int ic_launcher2=0x7f02005a;
->>>>>>> Marcus
     }
     public static final class id {
         public static final int action_bar=0x7f05001c;
@@ -1113,10 +1102,6 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-<<<<<<< HEAD
-        public static final int action_settings=0x7f05003e;
-        public static final int activity_chooser_view_content=0x7f050025;
-=======
         public static final int action_settings=0x7f050073;
         public static final int activity_chat_room=0x7f05003f;
         public static final int activity_chooser_view_content=0x7f050025;
@@ -1127,21 +1112,15 @@ containing a value of this type.
         public static final int activity_topic_for_an_stock=0x7f050062;
         public static final int addBookmark=0x7f05005e;
         public static final int addThread=0x7f050064;
->>>>>>> Marcus
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
-<<<<<<< HEAD
-        public static final int container=0x7f05003c;
-        public static final int default_activity_button=0x7f050028;
-=======
         public static final int createevent=0x7f05006e;
         public static final int dateEditText=0x7f050072;
         public static final int default_activity_button=0x7f050028;
         public static final int delete=0x7f05005f;
         public static final int detail=0x7f050066;
->>>>>>> Marcus
         public static final int dialog=0x7f05000e;
         public static final int disableHome=0x7f050008;
         public static final int dropdown=0x7f05000f;
@@ -1149,26 +1128,12 @@ containing a value of this type.
         public static final int end=0x7f050013;
         public static final int expand_activities_button=0x7f050026;
         public static final int expanded_menu=0x7f05002c;
-<<<<<<< HEAD
-=======
         public static final int findThread=0x7f050061;
->>>>>>> Marcus
         public static final int home=0x7f050014;
         public static final int homeAsUp=0x7f050005;
         public static final int icon=0x7f05002a;
         public static final int ifRoom=0x7f05000a;
         public static final int image=0x7f050027;
-<<<<<<< HEAD
-        public static final int listMode=0x7f050001;
-        public static final int list_item=0x7f050029;
-        public static final int middle=0x7f050012;
-        public static final int never=0x7f050009;
-        public static final int none=0x7f050010;
-        public static final int normal=0x7f050000;
-        public static final int progress_circular=0x7f050018;
-        public static final int progress_horizontal=0x7f050019;
-        public static final int radio=0x7f05002f;
-=======
         public static final int intro=0x7f05005a;
         public static final int item=0x7f050065;
         public static final int layout1=0x7f05004d;
@@ -1202,7 +1167,6 @@ containing a value of this type.
         public static final int searchStock=0x7f05004b;
         public static final int searchStockName=0x7f05004a;
         public static final int searchThread=0x7f05003e;
->>>>>>> Marcus
         public static final int search_badge=0x7f050032;
         public static final int search_bar=0x7f050031;
         public static final int search_button=0x7f050033;
@@ -1213,24 +1177,12 @@ containing a value of this type.
         public static final int search_plate=0x7f050036;
         public static final int search_src_text=0x7f050037;
         public static final int search_voice_btn=0x7f05003b;
-<<<<<<< HEAD
-=======
         public static final int sendMessage=0x7f050042;
->>>>>>> Marcus
         public static final int shortcut=0x7f05002e;
         public static final int showCustom=0x7f050007;
         public static final int showHome=0x7f050004;
         public static final int showTitle=0x7f050006;
         public static final int split_action_bar=0x7f05001e;
-<<<<<<< HEAD
-        public static final int submit_area=0x7f050039;
-        public static final int tabMode=0x7f050002;
-        public static final int textView1=0x7f05003d;
-        public static final int title=0x7f05002b;
-        public static final int top_action_bar=0x7f050020;
-        public static final int up=0x7f050021;
-        public static final int useLogo=0x7f050003;
-=======
         public static final int stockName=0x7f05004e;
         public static final int stockNameResult=0x7f05004f;
         public static final int submit_area=0x7f050039;
@@ -1254,7 +1206,6 @@ containing a value of this type.
         public static final int vote=0x7f050067;
         public static final int voteResult=0x7f050068;
         public static final int whenText=0x7f050070;
->>>>>>> Marcus
         public static final int withText=0x7f05000c;
     }
     public static final class integer {
@@ -1281,10 +1232,7 @@ containing a value of this type.
          showAsAction="ifRoom" items can fit. "always" items can override this. 
          */
         public static final int abc_max_action_buttons=0x7f090000;
-<<<<<<< HEAD
-=======
         public static final int single_line=0x7f090001;
->>>>>>> Marcus
     }
     public static final class layout {
         public static final int abc_action_bar_decor=0x7f030000;
@@ -1311,14 +1259,6 @@ containing a value of this type.
         public static final int abc_search_dropdown_item_icons_2line=0x7f030015;
         public static final int abc_search_view=0x7f030016;
         public static final int abc_simple_decor=0x7f030017;
-<<<<<<< HEAD
-        public static final int activity_main=0x7f030018;
-        public static final int fragment_main=0x7f030019;
-        public static final int support_simple_spinner_dropdown_item=0x7f03001a;
-    }
-    public static final class menu {
-        public static final int main=0x7f0c0000;
-=======
         public static final int activity_bookmark_list=0x7f030018;
         public static final int activity_chat_room=0x7f030019;
         public static final int activity_main=0x7f03001a;
@@ -1342,7 +1282,6 @@ containing a value of this type.
         public static final int third=0x7f0c0006;
         public static final int topic_for_an_stock=0x7f0c0007;
         public static final int topic_for_an_stock_adapter=0x7f0c0008;
->>>>>>> Marcus
     }
     public static final class string {
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
@@ -1385,11 +1324,6 @@ containing a value of this type.
         /**  Description of a share target (both in the list of such or the default share button) in a ShareActionProvider (share UI). [CHAR LIMIT=NONE] 
          */
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000b;
-<<<<<<< HEAD
-        public static final int action_settings=0x7f0a000f;
-        public static final int app_name=0x7f0a000d;
-        public static final int hello_world=0x7f0a000e;
-=======
         public static final int action_settings=0x7f0a0010;
         public static final int app_name=0x7f0a000d;
         public static final int hello_world=0x7f0a000f;
@@ -1402,7 +1336,6 @@ containing a value of this type.
         public static final int title_activity_third=0x7f0a0012;
         public static final int title_activity_topic_for_an_stock=0x7f0a0014;
         public static final int title_activity_topic_for_an_stock_adapter=0x7f0a0015;
->>>>>>> Marcus
     }
     public static final class style {
         /** 
